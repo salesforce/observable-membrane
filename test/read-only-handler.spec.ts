9c54ec0003cf5bbf5933628c496b5ef3e0f55ae3
Caridy Patiño <caridy@gmail.com>
2018-07-31 17:10:52 -0400

    perf: fix for small perf regression associated to defineProperties().

--- test/read-only-handler.spec.ts
+++ test/read-only-handler.spec.ts
@@ -271,4 +271,22 @@ describe('ReadOnlyHandler', () => {
             doNothing(readOnly.foo);
         }).not.toThrow();
     });
+    it('should throw when attempting to change the prototype', function() {
+        const target = new ReactiveMembrane();
+        const obj = { foo: 'bar' };
+
+        const property = target.getReadOnlyProxy(obj);
+        expect(() => {
+            Object.setPrototypeOf(property, {});
+        }).toThrow();
+    });
+    it('should throw when attempting to change the prototype of a member property', function() {
+        const target = new ReactiveMembrane();
+        const obj = { foo: { bar: 'baz' } };
+
+        const property = target.getReadOnlyProxy(obj);
+        expect(() => {
+            Object.setPrototypeOf(property.foo, {});
+        }).toThrow();
+    });
 });
