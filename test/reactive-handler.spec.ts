d64791f0d57d25c702e283db4a47aae9b68b8a08
Nolan Lawson <nlawson@salesforce.com>
2021-10-29 12:29:41 -0700

    perf: avoid creating readonly handler unnecessarily

--- test/reactive-handler.spec.ts
+++ test/reactive-handler.spec.ts
@@ -1028,4 +1028,9 @@ describe('ReactiveHandler', () => {
             expect((dry as any).$$MagicKey$$).toBe('bar');
         });
     });
+    it('should return undefined when input is undefined', () => {
+        const target = new ReactiveMembrane();
+        const state = target.getProxy(undefined);
+        expect(state).toBe(undefined);
+    });
 });
